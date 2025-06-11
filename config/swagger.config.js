045df4cc7ac78f01b7bf3307828ebadf62cd0bcf
HCM24_CPL_NET_14_HaiSon <sonthse172913@fpt.edu.vn>
2025-06-11 09:46:39 +0700

    change swagger url

--- config/swagger.config.js
+++ config/swagger.config.js
@@ -42,7 +42,7 @@ const options = {
     servers: [
       {
         // url: 'http://localhost:3000/',
-        url: 'https://smoking-cessation-backend.onrender.com/',
+        url: 'https://smoking-cessation-backend.onrender.com/', 
       },
     ],
   },
