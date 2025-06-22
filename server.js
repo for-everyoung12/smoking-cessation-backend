54f83648a35dc7e6406c5001b349a50369a08745
Tran Hai Son <sonthse172913@fpt.edu.vn>
2025-06-22 21:34:41 +0700

    create blog, chat with coach

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-// server.js
+const express = require('express');
 const app = require('./app');
 const http = require('http');
 const setupCommunityChat = require('./socket/communityChat');
+const setupChatSession = require('./socket/chatSession');
 
-const server = http.createServer(app); 
-setupCommunityChat(server);            
+const server = http.createServer(app);
+
+app.use(express.static('public'));
+
+// Setup community chat socket
+setupCommunityChat(server);
+
+// Setup chat session socket (PHẦN MỚI)
+setupChatSession(server);
 
 const port = process.env.PORT || 3000;
 
