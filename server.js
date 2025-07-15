8f556cf45217102a613b80eb9441d3f71bf15c23
Tuan Anh <anhlvtse172914@fpt.edu.vn>
2025-07-15 16:31:22 +0700

    update swagger

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const http = require('http');
 const express = require('express');
 const app = require('./app');
 const setupSocket = require("./socket");
-const { setSocketIO } = require("./utils/notify"); // ✅ THÊM DÒNG NÀY
+const { setSocketIO } = require("./utils/notify");
 
 const server = http.createServer(app); 
 
-const io = setupSocket(server); // ✅ LẤY socket instance
-setSocketIO(io);                // ✅ GÁN vào utils để cron dùng được
+const io = setupSocket(server); 
+setSocketIO(io);                
 
 
 
